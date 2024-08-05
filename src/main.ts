8ff0d076a7f7c7553cc77dde3c3e1eb670b5fad8
Oh Jisu <parangwavetraveller@gmail.com>
2024-08-05 23:41:10 +0900

    chore: Swagger api 문서 생성
    Swagger api 문서 생성

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Swagger 설정
+  const config = new DocumentBuilder()
+    .setTitle('Community Service API')
+    .setDescription('The community service API description')
+    .setVersion('1.0')
+    .addTag('users')
+    .build();
+
+  // config 바탕 swagger doc 생성
+  const document = SwaggerModule.createDocument(app, config);
+
+  // Swagger UI를 위한 path 연결
+  // .setup('swagger ui endpoint', app, swagger_document)
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
